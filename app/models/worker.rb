553de6492ad192a18724751efb5c4ac6e3d75ed0
gauravbestpeer <gapal@bestpeers.com>
2023-01-17 19:37:28 +0530

    UI and basic authentication completed

---
+++ app/models/worker.rb
@@ -0,0 +1,16 @@
+class Worker < ApplicationRecord
+    validates :name,:date , presence: true
+    before_create :check_for_name_and_date
+
+    def check_for_name_and_date
+        puts self.name,self.date
+        
+        if Worker.find_by(name: self.name ,date: self.date)
+            byebug
+            self.name = nil
+            self.date = nil
+        end
+
+    end
+
+end
